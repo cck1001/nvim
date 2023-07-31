d2cc858e954b958e9589151c190af93beb41cf8d
cck <vogo0410@126.com>
2023-07-31 21:23:33 +0800

    first commit

---
+++ lua/plugins/cck.lua
@@ -0,0 +1,185 @@
+return {
+  { "theniceboy/nvim-deus" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "deus",
+    },
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { pattern = "*", command = "EnableHL" })
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          use_treesitter = true,
+          style = {
+            { fg = "#806d9c" },
+          },
+        },
+        indent = {
+          chars = { "│", "¦", "┆", "┊" },
+          use_treesitter = false,
+        },
+        blank = {
+          enable = false,
+        },
+        line_num = {
+          use_treesitter = true,
+        },
+      })
+    end,
+  },
+  { "Bekaboo/dropbar.nvim" },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            bt_ignore = { "terminal" },
+            ft_ignore = { "oil" },
+            segments = {
+              {
+                sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+                click = "v:lua.ScSa",
+              },
+              { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+              {
+                sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true },
+                click = "v:lua.ScSa",
+              },
+              {
+                text = { " ", builtin.foldfunc, " " },
+                condition = { builtin.not_empty, true, builtin.not_empty },
+                click = "v:lua.ScFa",
+              },
+            },
+          })
+        end,
+      },
+    },
+    event = "BufRead",
+    opts = function(_, opts)
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (" 󱞡 %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
+
+      opts.fold_virt_text_handler = handler
+
+      -- NOTE: https://github.com/kevinhwang91/nvim-ufo/issues/72
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require("lspconfig")[ls].setup({
+          capabilities = capabilities,
+        })
+      end
+    end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      position = "right",
+      auto_preview = true,
+      symbols = {
+        File = { icon = "", hl = "@text.uri" },
+        Module = { icon = "", hl = "@namespace" },
+        Namespace = { icon = "", hl = "@namespace" },
+        Package = { icon = "", hl = "@namespace" },
+        Class = { icon = "", hl = "@type" },
+        Method = { icon = "ƒ", hl = "@method" },
+        Property = { icon = "", hl = "@method" },
+        Field = { icon = "", hl = "@field" },
+        Constructor = { icon = "", hl = "@constructor" },
+        Enum = { icon = "", hl = "@type" },
+        Interface = { icon = "", hl = "@type" },
+        Function = { icon = "", hl = "@function" },
+        Variable = { icon = "", hl = "@constant" },
+        Constant = { icon = "", hl = "@constant" },
+        String = { icon = "", hl = "@string" },
+        Number = { icon = "#", hl = "@number" },
+        Boolean = { icon = "", hl = "@boolean" },
+        Array = { icon = "", hl = "@constant" },
+        Object = { icon = "", hl = "@type" },
+        Key = { icon = "", hl = "@type" },
+        Null = { icon = "", hl = "@type" },
+        EnumMember = { icon = "", hl = "@field" },
+        Struct = { icon = "", hl = "@type" },
+        Event = { icon = "", hl = "@type" },
+        Operator = { icon = "", hl = "@operator" },
+        TypeParameter = { icon = "", hl = "@parameter" },
+        Component = { icon = "", hl = "@function" },
+        Fragment = { icon = "", hl = "@constant" },
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {
+      use_diagnostic_signs = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                composites = false,
+                fieldalignment = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
